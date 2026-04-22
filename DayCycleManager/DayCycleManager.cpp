7882dc9d93b4b1c95592fafedbb3fcd2dbe0cc07
fatihserenli-rgb <fatihserenli55@gmail.com>
2026-04-22 23:35:05 +0300

    Add files via upload

---
+++ DayCycleManager/DayCycleManager.cpp
@@ -0,0 +1,53 @@
+﻿#include "DayCycleManager.h"
+#include "Engine/DirectionalLight.h"
+
+ADayCycleManager::ADayCycleManager()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
+	PrimaryActorTick.TickGroup = TG_PrePhysics;
+	Hour = 12;
+	Minute = 0.0f;
+}
+
+void ADayCycleManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	PrimaryActorTick.TickInterval = ((DayLength * 60.0f) / 360.0f) / 2;
+}
+
+void ADayCycleManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	Minute += 2;
+	if (Minute >= 60)
+	{
+		Minute -= 60;
+		Hour++;
+		OnHourChanged.Broadcast(Hour);
+		switch (Hour)
+		{
+		case 0:
+			OnNightStarted.Broadcast();
+			break;
+		case 6:
+			OnMorningStarted.Broadcast();
+			break;
+		case 12:
+			OnNoontStarted.Broadcast();
+			break;
+		case 18:
+			OnEveningtStarted.Broadcast();
+			break;
+		default:
+			break;
+		}
+	}
+	if (Hour >= 24)
+	{
+		Hour = 0;
+	}
+	UE_LOG(LogTemp, Log, TEXT("Hour: %d, Minute = %d"), Hour,Minute);
+	Sun->AddActorLocalRotation(FRotator(0.5f, 0.0f, 0.0f));
+}
---
+++ DayCycleManager/DayCycleManager.h
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DayCycleManager.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHourChanged, int32, Hour);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnMorningStarted);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnNoontStarted);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnEveningtStarted);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnNightStarted);
+
+class ADirectionalLight;
+
+UCLASS()
+class STAJ_API ADayCycleManager : public AActor
+{
+    GENERATED_BODY()
+    
+public:
+    ADayCycleManager();
+    
+    virtual void BeginPlay() override;
+    
+    virtual void Tick( float DeltaSeconds ) override;
+    UPROPERTY(BlueprintAssignable)
+    FOnHourChanged OnHourChanged;
+    
+    UPROPERTY(BlueprintAssignable)
+    FOnMorningStarted OnMorningStarted;
+    
+    UPROPERTY(BlueprintAssignable)
+    FOnNoontStarted OnNoontStarted;
+    
+    UPROPERTY(BlueprintAssignable)
+    FOnEveningtStarted OnEveningtStarted;
+    
+    UPROPERTY(BlueprintAssignable)
+    FOnNightStarted OnNightStarted;
+    
+private:
+    UPROPERTY(EditAnywhere)
+    ADirectionalLight *Sun;
+    
+    UPROPERTY(EditAnywhere)
+    int32 DayLength;
+    
+    int8 Hour;
+    int8 Minute;
+};
